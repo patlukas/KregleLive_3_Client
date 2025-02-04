patlukas/KregleLive_3_Client

commit 07cc5b432028c428b98026764e6110b990ec9ecf

Add a section to set the names of players playing in the "classic" game type

results_manager.py
@@ -38,7 +38,8 @@ class ResultsManager:
         self.__round: int = -1
         self.__status_on_lanes: list[list[int] | None] = [([] if l == 1 else None) for l in game_type.lanes]
         self.__results_container.init_struct(self.__game_type.number_team, self.__game_type.number_player_in_team_in_period)
-        if self.__game_type.type == "league":
+        self.__functions_wait_to_new_block: list[Callable] = []
+        if self.__game_type.type == "league": # TODO Przenieś to do PlayerSectionLEague
             for _ in range(self.__game_type.number_periods):
                 self.add_block("")
 
@@ -146,7 +147,8 @@ class ResultsManager:
                 self.__block_number += 1
                 self.__block_is_running = True
                 self.__on_add_log(3, "RST_BLOCK_NEW", f"", f"Rozpoczęto blok numer {self.__block_number + 1} na wszystkich torach", True)
-
+                for f in self.__functions_wait_to_new_block:
+                    f()
 
     def __check_end_block(self) -> bool:
         """
@@ -308,7 +310,7 @@ class ResultsManager:
         :return: False | tuple[int, tuple[int, int, int]] in tuple -> status on lane, and (team_index, player_index, lane_index)
                 if lane_index == -1 it means on lane is trial
         """
-        if self.__block_number == self.__game_type.number_periods - 1 and not self.__block_is_running:
+        if self.__block_number == self.__game_type.number_periods and not self.__block_is_running:
             return False
         status_on_lane = self.__status_on_lanes[lane]
         if status_on_lane is None or len(status_on_lane) == 0:
@@ -401,3 +403,37 @@ class ResultsManager:
         :return: dict - team/player/lane statistics
         """
         return [self.__results_container.get_dict_with_results(list_of_result_names, (0, 0, 0), 0)]
+
+    def get_number_of_blocks(self) -> int:
+        return len(self.__list_of_blocks)
+
+    def add_function_wait_to_new_block(self, func: Callable):
+        self.__functions_wait_to_new_block.append(func)
+
+    def get_player_name_in_relative_block(self, team: int, player: int, relative_block: int) -> str | None:
+        """
+        :param team: <int> team number
+        :param player: <int> relative player number (player in block)
+        :param relative_block: <int> 0 - actual block, -1 - previous block, 1 - next block
+        :return: str - str - player name, None - the expected block does not exist
+        """
+        block = self.__block_number + relative_block
+        if block < 0 or block >= self.get_number_of_blocks():
+            return None
+        previous_players = block * self.__game_type.number_player_in_team_in_period
+        return self.__results_container.get_player_name((team, player + previous_players))
+
+    def set_player_name_in_relative_block(self, team: int, player: int,  relative_block: int, name: str) -> bool:
+        """
+        :param team: <int> team number
+        :param player: <int> relative player number (player in block)
+        :param relative_block: <int> 0 - actual block, -1 - previous block, 1 - next block
+        :param name: <str> new player name
+        :return: True - player name is set, False - the expected block does not exist
+        """
+        block = self.__block_number + relative_block
+        if block < 0 or block >= self.get_number_of_blocks():
+            return False
+        previous_players = block * self.__game_type.number_player_in_team_in_period
+        self.__results_container.set_player_name((team, player + previous_players), name)
+        return True

game_type_manager.py
@@ -139,7 +139,7 @@ class GameTypesManager:
                 transitions[transition_name] = Transition(transition_name, transition, number_lane)
             self.__game_types[game_type_name] = GameType(game_type_name, raw_game_type["type"], raw_game_type["lanes"],
                                                          raw_game_type.get("number_of_changes", None),
-                                                         raw_game_type.get("number_periods", None), number_team,
+                                                         raw_game_type.get("number_periods", -1), number_team,
                                                          number_player_in_team, transitions, raw_game_type.get("default_transitions", None))
 
 class Transition:
@@ -150,8 +150,8 @@ class Transition:
 
 class GameType:
     def __init__(self, name: str, game_type: Literal["classic", "league"], lanes: list[Literal[0, 1]],
-                 number_of_changes: int | None, number_periods: int | None, number_team: int,
-                 number_player_in_team_in_period: int, transitions: dict[str: Transition], default_transitions: str | None):
+                 number_of_changes: int | None, number_periods: int, number_team: int,
+                 number_player_in_team_in_period: int, transitions: dict[str: Transition], default_transitions: str):
         self.name: str = name
         self.type: Literal["classic", "league"] = game_type
         self.lanes: list[Literal[0, 1]] = lanes

gui/player_section_classic.py
@@ -1,0 +1,148 @@
+from PyQt6.QtWidgets import QGroupBox, QLabel, QPushButton, QComboBox, QGridLayout
+from collections.abc import Callable
+
+from game_type_manager import GameType
+from player_licenses import PlayerLicenses
+from results_manager import ResultsManager
+
+
+class _SectionChooseTransition(QGroupBox):
+    def __init__(self, name: str, list_transitions: list[str], on_select_transition: Callable[[str], None]):
+        super().__init__(name)
+        self.__list_transitions: list[str] = list_transitions
+        self.__label: QLabel = QLabel("Rodzaj bloku")
+        self.__dropdown: QComboBox = QComboBox()
+        self.__btn: QPushButton = QPushButton("Wybierz")
+        self.__dropdown.addItems(list_transitions)
+        self.__btn.clicked.connect(lambda: on_select_transition(self.__dropdown.currentText()))
+
+        self.__layout = QGridLayout()
+        self.__layout.addWidget(self.__label, 0, 0)
+        self.__layout.addWidget(self.__dropdown, 0, 1)
+        self.__layout.addWidget(self.__btn, 0, 2)
+        self.setLayout(self.__layout)
+
+
+class _SectionSetName(QGroupBox):
+    def __init__(self, name: str, number_player: int, on_save_names: Callable[[list[str]], None], on_get_names: Callable[[], None | list[str]]):
+        super().__init__(name)
+        self.__on_save_names: Callable[[list[str]], None] = on_save_names
+        self.__on_get_names: Callable[[], None | list[str]] = on_get_names
+        self.__list_row: list[tuple[QLabel, QComboBox]] = []
+        self.__btn: QPushButton = QPushButton("Zapisz")
+        self.__btn.clicked.connect(self.__on_save)
+        self.__layout = QGridLayout()
+
+        for i in range(number_player):
+            l = QLabel(f"Tor {i+1}")
+            d: QComboBox = QComboBox()
+            d.setEditable(True)
+            self.__layout.addWidget(l, i, 0)
+            self.__layout.addWidget(d, i, 1)
+            self.__list_row.append((l, d))
+
+        self.__layout.addWidget(self.__btn, number_player, 1)
+        self.setLayout(self.__layout)
+
+    def refresh_list_players(self, list_players):
+        for _, dropdown in self.__list_row:
+            dropdown.clear()
+            dropdown.addItems(list_players)
+
+    def __on_save(self):
+        list_name = []
+        for _, dropdown in self.__list_row:
+            list_name.append(dropdown.currentText())
+        self.__on_save_names(list_name)
+
+    def load_players_data(self):
+        list_name = self.__on_get_names()
+        if list_name is None:
+            return
+        for i, name in enumerate(list_name):
+            self.__list_row[i][1].setCurrentText(name)
+
+
+class PlayersSectionClassic(QGroupBox):
+    """
+        TODO: Add comment
+    """
+    def __init__(self, results_manager: ResultsManager, game_type: GameType, player_licenses: PlayerLicenses,
+                 on_refresh_tables: Callable[[], None]):
+        super().__init__("Ustawianie nazw")
+        self.__on_refresh_tables: Callable[[], None] = on_refresh_tables
+        self.__results_manager: ResultsManager = results_manager
+        self.__player_licenses: PlayerLicenses = player_licenses
+        self.__transitions: list[str] = game_type.get_list_transitions_name()
+        self.__default_transitions: str = game_type.default_transitions
+        self.__number_player_in_period: int = game_type.number_player_in_team_in_period
+
+
+        self.__section_next_set_name: _SectionSetName = _SectionSetName(
+            "Następny blok",
+            self.__number_player_in_period,
+            lambda list_name: self.__set_players_names_in_relative_block(1, list_name),
+            lambda: self.__get_players_names_in_relative_block(1)
+        )
+        self.__section_now_set_name: _SectionSetName = _SectionSetName(
+            "Aktualny blok",
+            self.__number_player_in_period,
+            lambda list_name: self.__set_players_names_in_relative_block(0, list_name),
+            lambda: self.__get_players_names_in_relative_block(0)
+        )
+        self.__section_next_select_block: _SectionChooseTransition = _SectionChooseTransition("Następny blok", self.__transitions, self.__select_transition)
+
+        self.__layout = QGridLayout()
+        self.__layout.addWidget(self.__section_now_set_name, 0, 0)
+        self.__layout.addWidget(self.__section_next_set_name, 0, 1)
+        self.__layout.addWidget(self.__section_next_select_block, 0, 1)
+        self.setLayout(self.__layout)
+
+        self.on_after_new_block()
+        self.load_data_from_new_category()
+
+    def __select_transition(self, transition: str):
+        self.__results_manager.add_block(transition)
+        self.__section_next_set_name.load_players_data()
+        if self.__results_manager.get_number_of_blocks() == 1:
+            self.__section_now_set_name.setParent(None)
+            self.__layout.addWidget(self.__section_next_set_name, 0, 1)
+            self.__section_next_select_block.setParent(None)
+        else:
+            self.__layout.addWidget(self.__section_now_set_name, 0, 0)
+            self.__layout.addWidget(self.__section_next_set_name, 0, 1)
+            self.__section_next_select_block.setParent(None)
+
+    def on_after_new_block(self):
+        if len(self.__transitions) > 1:
+            if self.__results_manager.get_number_of_blocks() == 0:
+                self.__section_now_set_name.setParent(None)
+                self.__section_next_set_name.setParent(None)
+                self.__layout.addWidget(self.__section_next_select_block, 0, 1)
+            else:
+                self.__layout.addWidget(self.__section_now_set_name, 0, 0)
+                self.__section_next_set_name.setParent(None)
+                self.__layout.addWidget(self.__section_next_select_block, 0, 1)
+        else:
+            self.__select_transition(self.__transitions[0])
+        self.__section_now_set_name.load_players_data()
+
+    def load_data_from_new_category(self):
+        list_payers = self.__player_licenses.get_list_players_name(None)
+        self.__section_now_set_name.refresh_list_players(list_payers)
+        self.__section_next_set_name.refresh_list_players(list_payers)
+
+    def __set_players_names_in_relative_block(self, relative_block: int, list_name: list[str]) -> None:
+        for i, name in enumerate(list_name):
+            r = self.__results_manager.set_player_name_in_relative_block(0, i, relative_block, name)
+            print(i, name, relative_block, r)
+
+    def __get_players_names_in_relative_block(self, relative_block: int) -> list[str] | None:
+        list_name = []
+        for i in range(self.__number_player_in_period):
+            name = self.__results_manager.get_player_name_in_relative_block(0, i, relative_block)
+            if name is None:
+                return None
+            list_name.append(name)
+        return list_name
+

main.py
@@ -9,7 +9,8 @@ from category_type_manager import CategoryTypesManager, CategoryTypesManagerErro
 from create_result_table import CreateTableMain, CreateTableLane
 from game_type_manager import GameTypesManager, GameTypesManagerError
 from gui.logs_section import LogsSection
-from gui.players_section import PlayersSectionLeague
+from gui.player_section_classic import PlayersSectionClassic
+from gui.players_section_league import PlayersSectionLeague
 from gui.splash_screen import SplashScreen
 from gui.statistics_section import StatisticsSection
 from log_management import LogManagement
@@ -262,7 +263,8 @@ class Main(QWidget):
         if game_type.type == "league":
             self.__player_section = PlayersSectionLeague(self.__results_manager, game_type, self.__player_licenses, self.__on_refresh_tables)
         elif game_type.type == "classic":
-            self.__player_section = None # TODO
+            self.__player_section = PlayersSectionClassic(self.__results_manager, game_type, self.__player_licenses, self.__on_refresh_tables)
+            self.__results_manager.add_function_wait_to_new_block(self.__player_section.on_after_new_block)
 
         self.__column2_layout.addWidget(self.__player_section, 4, 0)
 

results_container.py
@@ -108,7 +108,7 @@ class ResultsContainer:
         """
         This method sets the player name
 
-        :param who: <tuple(int: number_team, int: number_player, int: number_lane)> if lane number is -1 it means trial
+        :param who: <tuple(int: number_team, int: number_player)>
         :param name: <str> player name
         :return: None
         """
@@ -118,7 +118,7 @@ class ResultsContainer:
         """
         This method set player name or player's if was playing more than one player
 
-        :param who: <tuple(int: number_team, int: number_player, int: number_lane)> if lane number is -1 it means trial
+        :param who: <tuple(int: number_team, int: number_player)>
         :param list_name: <list[tuple[str, int]]> player name and throw number when he started playing
         :return: None
         """
@@ -128,7 +128,7 @@ class ResultsContainer:
         """
         This method sets the previous sum e.g. the result from the elimination
 
-        :param who: <tuple(int: number_team, int: number_player, int: number_lane)> if lane number is -1 it means trial
+        :param who: <tuple(int: number_team, int: number_player)>
         :param previous_sum: <int> e.g. the result from the elimination
         :return: None
         """
@@ -185,6 +185,15 @@ class ResultsContainer:
         :return: <str> "" or value
         """
         return self.teams[int(team)].get_stat(player, lane, stat, option)
+
+    def get_player_name(self, who: tuple[int, int]) -> str:
+        """
+        This method gets the player name
+
+        :param who: <tuple(int: number_team, int: number_player)>
+        :return: str
+        """
+        return self.teams[who[0]].players[who[1]].get_name()
 
 
 class ResultsContainerLeague(ResultsContainer):

results_container_player.py
@@ -145,7 +145,7 @@ class ResultsContainerPlayer:
             if stat == "name_now_playing_player":
                 return self.list_name[-1][0]
             if stat == "name":
-                return self.__get_name()
+                return self.get_name()
             if stat == "previous_sum":
                 return str(self.previous_sum)
             if stat == "total_sum":
@@ -195,7 +195,7 @@ class ResultsContainerPlayer:
                 sum_v += v
         return sum_v
 
-    def __get_name(self) -> str:
+    def get_name(self) -> str:
         """
         This method return player name, i more then one player was playing, give first letter of firstname and surname, and separate players by "/"
 

commit 59ec2f980307de9a3bf0e018af7f6a4fa8d38a1e

Fix: bug introduced in 07cc5b4

results_manager.py
@@ -23,9 +23,9 @@ class ResultsManager:
 
         Info:
             1. variables "self.__block_number" and "self.__max_block_number" have equal values almost all the time.
-                "self.__block_is_running" is greater than "self.__block_number" by 1 only in
+                "self.__max_block_number" is greater than "self.__block_number" by 1 only in
                 the time between when a new block starts on some track and when a new block starts on all tracks
-            2. you cannot remove blocks whose indexes are less than or equal to "self.__block_is_running"
+            2. you cannot remove blocks whose indexes are less than or equal to "self.__max_block_number"
         # TODO: uproszczenie self.__status_on_lanes z list[int, ....] do list[int, int]
         """
         self.__results_container: ResultsContainer = results_container
@@ -310,7 +310,7 @@ class ResultsManager:
         :return: False | tuple[int, tuple[int, int, int]] in tuple -> status on lane, and (team_index, player_index, lane_index)
                 if lane_index == -1 it means on lane is trial
         """
-        if self.__block_number == self.__game_type.number_periods and not self.__block_is_running:
+        if self.__block_number == self.__game_type.number_periods - 1 and not self.__block_is_running:
             return False
         status_on_lane = self.__status_on_lanes[lane]
         if status_on_lane is None or len(status_on_lane) == 0: