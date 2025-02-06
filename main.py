59ea796a7bde4a27aa0dd46f32a8a40202be2d7e
patlukas <lukaszewski.patryk06@gmail.com>
2025-02-07 00:34:44 +0100

    Switching to python 13, closed #19, release version 1.1.2

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ from gui.game_type_section import GameTypeSection
 from gui.socket_selection import SocketSelection
 from socket_manager import SocketManager
 
-APP_VERSION = "1.1.1"
+APP_VERSION = "1.1.2"
 
 class WorkerThread(QThread):
     def __init__(self, log_management: LogManagement, socket_manager: SocketManager, messages_interpreter: MessagesInterpreter,
