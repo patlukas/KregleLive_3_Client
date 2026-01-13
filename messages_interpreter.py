bd885b5b79b287ef4473b716fd52bc77a6b0ec67
patluka <lukaszewski.patryk06@gmail.com>
2026-01-13 14:03:31 +0100

    Add more known messages

--- messages_interpreter.py
+++ messages_interpreter.py
@@ -115,6 +115,8 @@ class MessagesInterpreter:
             else:
                 self.__number_of_unrecognized_message += 1
                 self.__on_add_log(9, "INT_TOLANE_UNKNOWN", "", f"Unknown message to {recipient_str} with content: {content}", True)
+        elif x == b"T":
+            self.__on_add_log(0, "INT_TOLANE_IGNORE", "PANEL", f"In lane {recipient_str} was sent msg {content[1:3]}", False)
         else:
             self.__number_of_unrecognized_message += 1
             self.__on_add_log(9, "INT_TOLANE_UNKNOWN", "", f"Unknown message to {recipient_str} with content: {content}", True)
